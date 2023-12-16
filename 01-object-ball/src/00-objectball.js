3cfe3d2ebdb6545508293352d3bd8233b90dface
betalantz <betalantz@gmail.com>
2023-12-15 16:30:51 -0800

    adds bonus solutions, comments 01

--- 01-object-ball/src/00-objectball.js
+++ 01-object-ball/src/00-objectball.js
@@ -122,6 +122,9 @@ function allPlayersObject() {
   return { ...game.home.players, ...game.away.players };
 }
 
+// the logic in this function could have been used inside of numPointsScored()
+// and shoeSize(), but whenever you see repeated logic
+// you should try to abstract it into a reusable helper function
 function playerStats(playerName) {
   const allPlayers = allPlayersObject();
   return allPlayers[playerName];
@@ -148,24 +151,30 @@ function shoeSize(playerName) {
   return playerStats(playerName).shoe;
 }
 
-function findByTeamname(teamName){
+function findByTeamName(teamName){
     return teams.find((teamObj) => teamObj.teamName === teamName)
 }
 
 function teamColors(teamName) {
-    return findByTeamname(teamName).colors
+    return findByTeamName(teamName).colors
 }
 
 function playerNumbers(teamName) {
-    const playersObj = findByTeamname(teamName).players
+    const playersObj = findByTeamName(teamName).players
     const statsArr = Object.values(playersObj)
     return statsArr.map(statsObj => statsObj.number)
+    // return Object.values(findByTeamName(teamName).players).map(statsObj => statsObj.number)
+  // this could be a one-line function, but it starts to get difficult to read
 }
 
 function teamNames() {
-    return teams.map(function(teamObj){
-        return teamObj.teamName
-    })
+  // return teams.map(function(teamObj){  // syntax with anonymous function
+  //   return teamObj.teamName
+  // })
+
+  // return teams.map((team) => { return team.teamName}) // syntax with arrow function
+
+  return teams.map(teamObj => teamObj.teamName) // shortest syntax with arrow function
 }
 
 
@@ -183,8 +192,99 @@ function bigShoeRebounds() {
     return sortedPlayers[0].rebounds
 }
 
-playerStats("Brendan Hayword");
-console.log(
-  "🚀 ~ file: 00-objectball.js:157 ~ playerStats():",
-  playerStats("Brendan Hayword")
-);
+// BONUS
+
+function mostPointsScored(){
+    // get a matrix of all players (array of arrays) that will keep the player name associated with their stats, but in a sortable structure
+    let sortedPlayers = Object.entries(allPlayersObject())
+    sortedPlayers.sort((a,b) => {
+      if (a[1].points < b[1].points) return -1;
+      if (a[1].points > b[1].points) return 1;
+      return 0
+    })
+    // debugger
+    // the sort was ascending order, so we need to use the last index of the array to grab the last element
+    // since the last element is also an array, we need another square bracket to get the name instead of the stats object
+    return sortedPlayers[sortedPlayers.length - 1][0]
+  }
+  
+  function winningTeam(){
+    // I want to create an array of team objs. I want an arr so I can sort it by team scores
+    // I'll create my own objects, with just the team name and total score
+    // I can use Object.values and reduce()
+    // let teamsWithScores = [  // too much hard coding!
+    //   {
+    //     teamName: game.home.teamName,
+    //     score: Object.values(game.home.players).reduce((sum, playerStats) => sum + playerStats.points, 0)
+    //   },
+    //   {
+    //     teamName: game.away.teamName,
+    //     score: Object.values(game.away.players).reduce((sum, playerStats) => sum + playerStats.points, 0)
+    //   }
+    // ]
+    let teamsWithScores = teams.map(teamObj => { // ah, better! will work no matter how many teams, and 
+      return {                                    // I'm not repeating myself
+        teamName: teamObj.teamName,
+        score: Object.values(teamObj.players).reduce((sum, playerStats) => sum + playerStats.points, 0)
+      }
+    })
+    // debugger
+    // I'll sort my custom array by the score and then take the first element from the array
+    const winner = teamsWithScores.sort((a, b) => b.score - a.score)[0]
+    return winner.teamName // use .dotnotation to return the name of the winning team
+  }
+  
+  // this question is faulty in that there are two players 
+  // with equally long names for the longest criteron
+  function playerWithLongestName(){
+    // Object.keys returns an array of strings-the player names
+    // Then we sort those strings in the array by their length property
+    let sortedNames = Object.keys(players).sort((a, b) => b.length - a.length)
+    // We can grab the first element from the sorted array with [] index notation
+    return sortedNames[0]
+  }
+  
+  // SUPER BONUS
+  
+  function doesLongNameStealATon(){
+    let sortedPlayers = Object.entries(players)
+    // console.log('sortedPlayers: ', sortedPlayers);
+    sortedPlayers.sort((a, b) => {
+      if (a[1].steals < b[1].steals) return -1;
+      if (a[1].steals > b[1].steals) return 1;
+      return 0
+    })
+    const mostStealsPlayer = sortedPlayers[0][0]
+    return playerWithLongestName() == mostStealsPlayer
+  }
+  
+  // here's one I made up to use .filter()
+  // returns an array of the player names who have shoe sizes bigger than 15
+  function bigFeetPlayers(){
+    // Object.entries keeps player names associated with their stats
+    const playerArr = Object.entries(players)
+    // debugger
+    // .filter will return a new array of player arrays only with shoe > 15, then .map will transform that into an array of just the name strings
+    return playerArr.filter(player => player[1].shoe > 15).map(pArr => pArr[0])
+  }
+  
+  
+  
+  // Object.keys: http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/keys
+  // Object.values: http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/values
+  // Object.entries: http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries
+  // for...in loop: http://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Statements/for...in
+  
+  console.log('numPointsScored: ', numPointsScored("Brendan Hayword"))
+  console.log('shoeSize: ', shoeSize('Ben Gordon'));
+  console.log('playerStats: ', playerStats('Jeff Adrien'));
+  console.log('findByTeamName: ', findByTeamName('Brooklyn Nets'));
+  console.log('teamColors: ', teamColors('Charlotte Hornets'));
+  console.log('teamNames: ', teamNames());
+  console.log('playerNumbers: ', playerNumbers('Brooklyn Nets'));
+  console.log('bigShoeRebounds: ', bigShoeRebounds());
+  console.log('mostPointsScored: ', mostPointsScored());
+  console.log('winningTeam: ', winningTeam());
+  
+  // debugger
+  
