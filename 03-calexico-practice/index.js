b9d6e027d56af0aeb3bf242b51a3078676a32f57
betalantz <betalantz@gmail.com>
2023-12-20 15:24:31 -0800

    first deliv complete 03

--- 03-calexico-practice/index.js
+++ 03-calexico-practice/index.js
@@ -1,1 +1,28 @@
-// Write your code here...
+// Global variables
+const URL = "http://localhost:3000/menu"
+
+// DOM selectors
+const menuItems = document.querySelector("#menu-items")
+
+// Fetch functions
+function getAllDishes(url){
+    fetch(url)
+      .then(res => res.json())
+      .then(dishArr => {
+        dishArr.forEach(dishObj => renderMenu(dishObj));
+      })
+}
+
+// Render functions
+function renderMenu(dishObj){
+    const dish = document.createElement("span")
+    dish.textContent = dishObj.name
+    console.log("🚀 ~ file: index.js:20 ~ renderMenu ~ dish:", dish)
+    menuItems.append(dish)
+}
+
+
+// Event Listeners & Handlers
+
+// Initializer
+getAllDishes(URL)
---
+++ 03-calexico-practice/notes.md
@@ -0,0 +1,17 @@
+## Mantra
+1. FETCH the data
+2. SELECT the right DOM elements
+3. CREATE new elements (as needed)
+4. ATTACH event listeners (as needed)
+5. ASSIGN data to DOM elements
+6. APPEND new elements to the DOM
+
+## Core Deliverables
+As a user, I can:
+1. View all menu items in a Menu section on page load
+- [x] Fetch all dish data (and jsonify)
+- [x] Select `menu-items`
+- [x] Iterate over array objects
+- [x] Create <span> for each obj
+- [x] Assign dish name to <span>
+- [x] Append <span> to `menu-items`
