14dc2515ba24b9d2385772906e186087af785556
betalantz <betalantz@gmail.com>
2024-07-01 14:43:44 -0700

    completes columns and migrations \

--- 15-flask-react-mini-challenge-cheesemakers/server/models.py
+++ 15-flask-react-mini-challenge-cheesemakers/server/models.py
@@ -1,24 +1,52 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import MetaData
 from sqlalchemy.ext.associationproxy import association_proxy
 from sqlalchemy.orm import validates
 from sqlalchemy_serializer import SerializerMixin
 
-db = SQLAlchemy()
+convention = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+metadata = MetaData(naming_convention=convention)
+
+db = SQLAlchemy(metadata=metadata, engine_options={"echo": True})
 
 
-class Producer(db.Model, SerializerMixin):
+class TimestampMixin:
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, server_onupdate=db.func.now())
+
+
+class Producer(db.Model, SerializerMixin, TimestampMixin):
     __tablename__ = "producers"
 
     id = db.Column(db.Integer, primary_key=True)
+    founding_year = db.Column(db.Integer)
+    name = db.Column(db.String, nullable=False)
+    region = db.Column(db.String)
+    operation_size = db.Column(db.String)
+    image = db.Column(db.String)
 
     def __repr__(self):
         return f"<Producer {self.id}>"
 
 
-class Cheese(db.Model, SerializerMixin):
+class Cheese(db.Model, SerializerMixin, TimestampMixin):
     __tablename__ = "cheeses"
 
     id = db.Column(db.Integer, primary_key=True)
+    kind = db.Column(db.String)
+    is_raw_milk = db.Column(db.Boolean)
+    production_date = db.Column(db.DateTime)
+    image = db.Column(db.String)
+    price = db.Column(db.Float)
+
+    producer_id = db.Column(db.Integer, db.ForeignKey("producers.id"), nullable=False)
 
     def __repr__(self):
         return f"<Cheese {self.id}>"
